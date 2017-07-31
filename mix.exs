ede9c8f3a2d15b131164764d234eae443efa7001
Moski Doski <abushaikh@gmail.com>
2017-07-31 18:46:06 +0400

    Adding Travis CI and cleaning the application config

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule Expander.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [app: :expander,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.4",
+     elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     aliases: aliases(),]
   end
 
   # Configuration for the OTP application
@@ -19,6 +23,9 @@ defmodule Expander.Mixfile do
      mod: {Expander.Application, []}]
   end
 
+  defp elixirc_paths(:test), do: ["lib"]
+  defp elixirc_paths(_),     do: ["lib"]
+
   # Dependencies can be Hex packages:
   #
   #   {:my_dep, "~> 0.3.0"}
@@ -29,6 +36,27 @@ defmodule Expander.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpotion, "~> 3.0.2"}]
+    [{:httpotion, "~> 3.0.2"},
+     {:ex_doc, "~> 0.15", only: :docs},
+     {:inch_ex, ">= 0.0.0", only: :docs}]
+  end
+
+
+  defp aliases do
+    ["test.ci": &test_ci/1]
+  end
+
+  defp test_ci(args) do
+    args = if IO.ANSI.enabled?, do: ["--color"|args], else: ["--no-color"|args]
+    args = if System.get_env("TRAVIS_SECURE_ENV_VARS") == "true", do: ["--include=integration"|args], else: args
+
+    {_, res} = System.cmd("mix",
+                          ["test"|args],
+                          into: IO.binstream(:stdio, :line),
+                          env: [{"MIX_ENV", "test"}])
+
+    if res > 0 do
+      System.at_exit(fn _ -> exit({:shutdown, 1}) end)
+    end
   end
 end
