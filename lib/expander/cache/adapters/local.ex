ee0dda03a5e0b5185b21697d11bd63ccea8c06e5
Moski Doski <abushaikh@gmail.com>
2017-08-08 16:19:34 +0400

    Adding tests for local adapter

--- lib/expander/cache/adapters/local.ex
+++ lib/expander/cache/adapters/local.ex
@@ -1,6 +1,7 @@
 defmodule Expander.Cache.Adapter.Local do
   use Expander.Cache.Adapter
   alias Expander.Cache.Store
+  alias Expander.Cache.Adapters.Local.Storage.Memory
 
   def setup(_) do
     Expander.Cache.Adapters.Local.Storage.Memory.start_link()
@@ -9,9 +10,18 @@ defmodule Expander.Cache.Adapter.Local do
   @spec get(Expander.Cache.Store.t, Expander.key)
     :: {:ok, Expander.Cache.Store.t, {:ok, Expander.value} | :error} | Expander.exception
   def get(store = %Store{state: conn}, key) do
-    case  Redix.command(conn, ["GET", key]) do
+    case  Memory.get(conn, key) do
       {:ok, nil}       -> {:ok, store, :error}
       {:ok, value}     -> {:ok, store, {:ok, value}}
+      {:error, reason} -> {:raise, Exception, [reason: reason]}
+    end
+  end
+
+  @spec set(Expander.Cache.Store.t, Expander.key, Expander.value)
+    :: {:ok, Expander.Cache.Store.t} | Expander.exception
+  def set(store = %Store{state: conn}, key, value) do
+    case  Memory.set(conn, key, value) do
+      {:ok, "OK"}      -> {:ok, store}
       {:error, reason} -> {:raise, Exception, [reason: reason]}
     end
   end
--- test/expander/cache/adapters/local_test.exs
+++ test/expander/cache/adapters/local_test.exs
@@ -1,34 +1,34 @@
-# defmodule Expander.Cache.Adapter.LocalTest do
-#   use ExUnit.Case, async: true
-#   defmodule LocalCache do
-#     use Expander.Expand, otp_app: :expander, adapter: Expander.Cache.Adapter.Local
-#   end
+defmodule Expander.Cache.Adapter.LocalTest do
+  use ExUnit.Case, async: true
 
-#   setup_all do
-#     valid_url = Expander.Url.new(
-#       short_url: "http://stpz.co/haddafios",
-#       long_url: "https://itunes.apple.com/us/app/haddaf-hdaf/id872585884",
-#     )
-#     LocalCache.adapter.set(valid_url, [])
-#     {:ok, valid_url: valid_url}
-#   end
+  alias Expander.Cache.Adapters.Local.Storage.Memory
 
 
-#   test "set/2" do
-#     url = Expander.Url.new(short_url: "http://amzn.to/2w9oM5d", long_url: "http://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/create_dashboard.html?sc_channel=sm&sc_campaign=Docs&sc_publisher=TWITTER&sc_country=Global&sc_geo=GLOBAL&sc_outcome=[GLOBAL]&sc_content=Docs&linkId=40350988")
-#     {:ok, {expanded, config}} = LocalCache.adapter.set(url, [])
-#     assert {:ok, {expanded, config}} == {:ok, {url, []}}
-#   end
+  defmodule LocalCache do
+    use Expander.Expand, otp_app: :expander, adapter: Expander.Cache.Adapter.Local
+  end
 
-#   test "get/2 returns error if the url not in cache" do
-#     url = Expander.Url.new(short_url: "http://buff.ly/2eXEo8I")
-#     assert {:error, :url_not_found} == LocalCache.adapter.get(url, [])
-#   end
+  setup_all do
+    {:ok, pid} = LocalCache.start_adapter()
+    state = :sys.get_state(pid)
+    %Expander.Cache.Store{state: conn} =  state
 
-#   test "get/2 returns url if in cache", %{valid_url: valid_url} do
-#     url = Expander.Url.new(short_url: "http://stpz.co/haddafios")
-#     {:ok, {expanded, config}} = LocalCache.adapter.get(url, [])
-#     assert {:ok, {expanded, config}} == {:ok, {valid_url, []}}
-#   end
+    # Make sure to flush all cache before we start.
+    Memory.delete_all(conn)
+    {:ok, state: state, conn: conn}
+  end
 
-# end
+  test "get/2", %{state: state, conn: conn}   do
+    assert Expander.Cache.Adapter.Local.get(state, "key") == {:ok, state, :error}
+
+    Memory.set(conn, "key", "value")
+
+    assert Expander.Cache.Adapter.Local.get(state, "key") == {:ok, state, {:ok, "value"}}
+  end
+
+  test "set/2", %{state: state, conn: conn}   do
+    assert {:ok, state} == Expander.Cache.Adapter.Local.set(state, "key1", "value1")
+    assert Memory.get(conn, "key1") == {:ok, "value1"}
+  end
+
+end
